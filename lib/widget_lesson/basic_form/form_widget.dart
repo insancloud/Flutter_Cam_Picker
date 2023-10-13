198022d3a6eec22d5e95e9ec11481799050749bb
insancloud <insan5272@icloud.com>
2023-10-14 01:41:52 +0800

    image picker

---
+++ lib/widget_lesson/basic_form/form_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ColumnWidget extends StatefulWidget {
+  const ColumnWidget({super.key});
+
+  @override
+  State<ColumnWidget> createState() => _ColumnWidgetState();
+}
+
+class _ColumnWidgetState extends State<ColumnWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
